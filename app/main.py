ff3c7a863fef5bf33bb1efb767b275f43370526a
sarangraii <sarang2452@gmail.com>
2025-12-25 19:27:24 +0530

    Allow Vercel frontend CORS

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ app = FastAPI(
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+        "https://health-fitness-tracker-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
